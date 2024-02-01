688baf84352c84f799e09d805153e5458c2f1b65
Diego Vilhalva <89986274+diegovilhalva@users.noreply.github.com>
2024-02-01 09:13:43 -0300

    Update config.php

--- config.php
+++ config.php
@@ -2,7 +2,7 @@
 
     $db_name = "mysql:host=localhost;dbname=shop_db";
     $username = "root";
-    $password = "#Hella2814";
+    $password = "";
 
     $conn = new PDO($db_name, $username, $password);
 ?>
